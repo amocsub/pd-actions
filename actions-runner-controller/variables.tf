2f87d7c3a6df03e0799a638c0f434d52747a3202
matias.busco <matias.busco@king.com>
2023-06-13 15:30:14 +0200

    remove

--- actions-runner-controller/variables.tf
+++ actions-runner-controller/variables.tf
@@ -1,20 +1,19 @@
-# variable "github-app-id" {
-#   default     = "345920"
-#   description = "The ID of your GitHub App."
-# }
+variable "github-app-id" {
+  default     = "345920"
+  description = "The ID of your GitHub App."
+}
 
-# variable "github-app-installation-id" {
-#   default     = "38498118"
-#   description = "The ID of your GitHub App installation."
-# }
+variable "github-app-installation-id" {
+  default     = "38498118"
+  description = "The ID of your GitHub App installation."
+}
 
-# variable "github-app-private-key-file" {
-#   default     = "~/pd-actions.2023-06-11.private-key.pem"
-#   description = "Filename where the multiline string of your GitHub App's private key is"
-# }
-
+variable "github-app-private-key-file" {
+  default     = ""
+  description = "Filename where the multiline string of your GitHub App's private key is"
+}
 
 variable "github-personal-access-token" {
-    default = "***REMOVED***"
+    default = ""
     description = "Github PAT to set-up the runners"
 }
